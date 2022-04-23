7cf3005659ead54ec848d0006bf7c719ecf9928e
Alvin <bioluminescencecloud@gmail.com>
2022-04-23 16:48:56 +0800

    Add server code

---
+++ src/server.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "You have requested: %s\n", r.URL.Path)
+	})
+
+	http.ListenAndServe(":443", nil)
+
+}
